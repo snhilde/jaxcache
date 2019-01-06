7743b568e62eef98e001d18d98c0981e218a5767
Hilde N <snhilde@protonmail.com>
2019-01-06 10:35:11 -0500

    define structs

--- jax_cache.h
+++ jax_cache.h
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _jax_cache {
-	} jax_cache;
+typedef struct _jax_list {
+	char *URL;
+	struct _jax_list *prev;
+	struct _jax_list *next;
+	} jax_list;
 
 typedef struct _jax_hash {
+	jax_list **list;
 	} jax_hash;
+
+typedef struct _jax_cache {
+	jax_hash *hash;
+	} jax_cache;
+
