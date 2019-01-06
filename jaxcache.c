8a1cb0634219ed5792d14beed42416916e3395e8
Hilde N <snhilde@protonmail.com>
2019-01-06 17:25:28 -0500

    change size to num_elem in jaxcache_create()

--- jaxcache.c
+++ jaxcache.c
@@ -28,7 +28,7 @@ int jaxcache_create(jaxcache **cache, size_t capacity)
 	(*cache)->head = NULL;
 	(*cache)->tail = NULL;
 	
-	(*cache)->size = 0;
+	(*cache)->num_elem = 0;
 	(*cache)->capacity = capacity;
 	
 	return 0;
