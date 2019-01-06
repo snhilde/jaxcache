3de9384046685f7df87164ea86f7d5c0a1d0b92f
Hilde N <snhilde@protonmail.com>
2019-01-06 15:52:41 -0500

    add 'size_t data_size' arg to jaxcache_add()

--- jaxcache.h
+++ jaxcache.h
@@ -6,7 +6,7 @@ typedef struct _jaxcache jaxcache;
 int jaxcache_create(jaxcache **cache, size_t max_capacity);
 void jaxcache_destroy(jaxcache *cache);
 
-int jaxcache_add(jaxcache *cache, char *URL, void *data);
+int jaxcache_add(jaxcache *cache, char *URL, void *data, size_t data_size);
 int jaxcache_lookup(jaxcache *cache, char *URL);
 
 size_t jaxcache_capacity(jaxcache *cache);
